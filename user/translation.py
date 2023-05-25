a5a8eb301a462691da3faecb6e3ba637b3ff9fb3
Samandar003 <samandar200527@gmail.com>
2023-05-25 12:37:13 -0400

    userapp changed

--- user/translation.py
+++
@@ -1,9 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from .models import MyModel
-
-class MyModelTranslationOptions(TranslationOptions):
-    fields = ('my_choice',)
-
-translator.register(MyModel, MyModelTranslationOptions)
-
-
