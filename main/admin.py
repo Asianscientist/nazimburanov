c9ba8c48c93bd1ee7f46cdb4306c58b67a8f7d46
Samandar003 <samandar200527@gmail.com>
2023-05-30 10:47:05 -0400

    admin registered

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Posts, VideoModel, BooksModel
+from .models import Posts, VideoModel, BooksModel, CertificateModel
 
 admin.site.register(Posts)
 admin.site.register(VideoModel)
 admin.site.register(BooksModel)
+admin.site.register(CertificateModel)
+
